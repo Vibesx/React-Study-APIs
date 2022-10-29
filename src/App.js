63555657f90dfd9474ca300a0dc7d71dc4f75db9
vibesx <lucian.paslariu@gmail.com>
2022-10-29 11:41:23 +0300

    added http request logic with then for promises

--- src/App.js
+++ src/App.js
@@ -1,34 +1,42 @@
-import React from 'react';
+import React from "react";
 
-import MoviesList from './components/MoviesList';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+	const [movies, setMovies] = useState([]);
+	function fetchMoviesHandler() {
+		// we can add a second parameter for fetch(), which can take an object of properties like header, body, method(GET, POST, etc), etc
+		// fetch docs: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API
+		fetch("https://swapi.dev/api/films")
+			.then((response) => {
+				// response.json() returns a promise, so we can add another .then after this function to handle that promise
+				return response.json();
+			})
+			.then((data) => {
+				const transformedMovies = data.results.map((movieData) => {
+					return {
+						id: movieData.episode_id,
+						title: movieData.title,
+						openingText: movieData.opening_crawl,
+						releaseData: movieData.release_date,
+					};
+				});
+				setMovies(transformedMovies);
+			});
+	}
 
-  return (
-    <React.Fragment>
-      <section>
-        <button>Fetch Movies</button>
-      </section>
-      <section>
-        <MoviesList movies={dummyMovies} />
-      </section>
-    </React.Fragment>
-  );
+	return (
+		<React.Fragment>
+			<section>
+				<button onClick={fetchMoviesHandler}>Fetch Movies</button>
+			</section>
+			<section>
+				<MoviesList movies={movies} />
+			</section>
+		</React.Fragment>
+	);
 }
 
 export default App;
