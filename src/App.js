bef1ddf60388eca5387ab94226a0d2e79767d296
vibesx <lucian.paslariu@gmail.com>
2022-10-29 11:46:26 +0300

    switched to async/await

--- src/App.js
+++ src/App.js
@@ -6,26 +6,42 @@ import { useState } from "react";
 
 function App() {
 	const [movies, setMovies] = useState([]);
-	function fetchMoviesHandler() {
-		// we can add a second parameter for fetch(), which can take an object of properties like header, body, method(GET, POST, etc), etc
-		// fetch docs: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API
-		fetch("https://swapi.dev/api/films")
-			.then((response) => {
-				// response.json() returns a promise, so we can add another .then after this function to handle that promise
-				return response.json();
-			})
-			.then((data) => {
-				const transformedMovies = data.results.map((movieData) => {
-					return {
-						id: movieData.episode_id,
-						title: movieData.title,
-						openingText: movieData.opening_crawl,
-						releaseData: movieData.release_date,
-					};
-				});
-				setMovies(transformedMovies);
-			});
+
+	// async/await approach:
+	async function fetchMoviesHandler() {
+		const response = await fetch("https://swapi.dev/api/films");
+		const data = await response.json();
+		const transformedMovies = data.results.map((movieData) => {
+			return {
+				id: movieData.episode_id,
+				title: movieData.title,
+				openingText: movieData.opening_crawl,
+				releaseData: movieData.release_date,
+			};
+		});
+		setMovies(transformedMovies);
 	}
+	// .then() approach:
+	// function fetchMoviesHandler() {
+	// 	// we can add a second parameter for fetch(), which can take an object of properties like header, body, method(GET, POST, etc), etc
+	// 	// fetch docs: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API
+	// 	fetch("https://swapi.dev/api/films")
+	// 		.then((response) => {
+	// 			// response.json() returns a promise, so we can add another .then after this function to handle that promise
+	// 			return response.json();
+	// 		})
+	// 		.then((data) => {
+	// 			const transformedMovies = data.results.map((movieData) => {
+	// 				return {
+	// 					id: movieData.episode_id,
+	// 					title: movieData.title,
+	// 					openingText: movieData.opening_crawl,
+	// 					releaseData: movieData.release_date,
+	// 				};
+	// 			});
+	// 			setMovies(transformedMovies);
+	// 		});
+	// }
 
 	return (
 		<React.Fragment>
